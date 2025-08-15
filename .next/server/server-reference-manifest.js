94951fd4686344e285d816eb254d76eab04b878d
Bismark Abban <abbanb2@gmail.com>
2025-08-15 02:30:15 +0000

    first-commit

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"w4qo7TYIcmf014JpXbZfQ0U9KyWmwDCkqefCDdVXrcY=\"}"
--- .next/static/chunks/app/legal/privacy-policy/layout-3483bcedf5c9d4a1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5238,3785,2855,2399,1140,7912,8567,6297,5365,1415],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
